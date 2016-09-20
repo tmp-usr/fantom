699bd26d912e92bad350bf9e3ce9924b60d80135
ali deniz <sanlik7@gmail.com>
2016-09-20 08:09:54 +0200

    relevant dirs moved.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='fantom',
+    version='0.0.2',
+    packages=['build.lib.fantom', 'build.lib.fantom.src', 'build.lib.fantom.src.helper', 'build.lib.fantom.src.sqling',
+              'build.lib.fantom.src.project', 'build.lib.fantom.src.analysis', 'build.lib.fantom.src.analysis.plotting',
+              'build.lib.fantom.src.launching', 'build.lib.fantom.src.comparison', 'fantom', 'fantom.gui.project',
+              'fantom.src', 'fantom.src.helper', 'fantom.src.sqling', 'fantom.src.project', 'fantom.src.analysis',
+              'fantom.src.analysis.old', 'fantom.src.analysis.plotting', 'fantom.src.launching',
+              'fantom.src.comparison', 'fantom.gui_old', 'fantom.gui_old.group', 'fantom.gui_old.group.objects',
+              'fantom.gui_old.stats', 'fantom.gui_old.trash', 'fantom.gui_old.trash.objects',
+              'fantom.gui_old.trash.objects_dev', 'fantom.gui_old.config', 'fantom.gui_old.helper',
+              'fantom.gui_old.models', 'fantom.gui_old.explore', 'fantom.gui_old.loaders', 'fantom.gui_old.abundance',
+              'fantom.gui_old.comparison', 'fantom.gui_old.comparison.helper', 'fantom.gui_old.newproject',
+              'fantom.gui_old.group_trash', 'fantom.gui_old.group_trash.objects', 'fantom.gui_old.project_panel'],
+    url='',
+    license='',
+    author='kemal',
+    author_email='kemalsanli1@gmail.com',
+    description=''
+)
